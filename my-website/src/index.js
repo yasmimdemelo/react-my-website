ded2ae185f520d25b65321797dfcdeb95a449d05
Yasmim <yasmimdmf95@gmail.com>
2023-01-09 16:45:03 -0300

    feat: add comments to understand how it works render react

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,4 +1,9 @@
-import { createRoot } from "react-dom/client";
+//react-dom faz com que possamos trabalhar com o cor do react na dom (web, mobile), aprendemos o cor do react e podemos utilizar em outros lugares
+//createroot é uma função js que vai receber quem é o elemento raiz "rootelement".
+//document.getElementById é um função que esta buscando no HTML, qual o elemento que tem o id = root.
+//rootelement armazena o valor encontrado na root, para usar no createRoot.
+//root vai nos retornar algumas opções uma delas é de renderizar um componente.
+import { createRoot } from "react-dom/client";  
 
 import App from "./App";
 
