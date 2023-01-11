7c322d2a6ac9add30262b7a6c54ef2170cd71a0b
Yasmim <yasmimdmf95@gmail.com>
2023-01-11 13:05:01 -0300

    feat: Add the path in the imgs and folders structure

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./assets/styles/styles.css";
 
 import Nav from "./components/Nav"
 import Header from "./components/Header"
--- my-website/src/components/Header.js
+++ my-website/src/components/Header.js
@@ -7,15 +7,15 @@ export default function Header (){
                     My mission is to develop websites with the purpose of helping people.</h3>
                 <p>I live in Florianópolis-SC, magic island.</p>
                 {/* rel="noreferrer" => evita passar as informações do referenciador para o site de destino removendo as informações de referência do cabeçalho HTTP.*/}
-                <a href="https://www.linkedin.com/in/yasmim-de-melo-a5036923b/"  rel="noreferrer"><img src="img/linkedin.png" alt="img linkedin"/></a>
-                <a href="https://github.com/yasmimdemelo"  rel="noreferrer"><img src="img/github-sign.png" alt="img github"/></a>
-                <a href="https://gitlab.com/yasmimdmf95"  rel="noreferrer"><img src="img/gitlab.png" alt="img gitlab"/></a>
-                <a href="https://www.instagram.com/yasmim_demelo/"  rel="noreferrer"><img src="img/instagram.png" alt="img instagram"/></a>
+                <a href="https://www.linkedin.com/in/yasmim-de-melo-a5036923b/"  rel="noreferrer"><img src="https://yasmimdemelo.com/img/linkedin.png" alt="Img Linkedin"/></a>
+                <a href="https://github.com/yasmimdemelo"  rel="noreferrer"><img src="https://yasmimdemelo.com/img/github-sign.png" alt="Img Github"/></a>
+                <a href="https://gitlab.com/yasmimdmf95"  rel="noreferrer"><img src="https://yasmimdemelo.com/img/gitlab.png" alt="Img Gitlab"/></a>
+                <a href="https://www.instagram.com/yasmim_demelo/"  rel="noreferrer"><img src="https://yasmimdemelo.com/img/instagram.png" alt="Img Instagram"/></a>
             </div>
 
             {/*Style inline é object js. Tem quer ser escrito in camel case e entre chaves*/}
             <div class="img-headline">
-                <img style={{ maxWidth: "90%" }} src="img/header.png" alt="img header"/>
+                <img style={{ maxWidth: "90%" }} src="https://yasmimdemelo.com/img/header.png" alt="img header"/>
             </div>
         </header>
     )
